f7f76febf676af2c0d744e11eda64780509b8077
chenlingjie <chenlingjie@cd121.com>
2018-01-17 16:39:59 +0800

    引入雪碧

---
+++ gulpfile.sprite.js
@@ -0,0 +1,24 @@
+/**
+ * Created by xiaobxia on 2018/1/3.
+ */
+let gulp = require('gulp');
+let spritesmith = require('gulp.spritesmith');
+
+gulp.task('sprite-2', function () {
+  let spriteData = gulp.src('src/asset/*-2x.png').pipe(spritesmith({
+    imgName: 'sprite-2.png',
+    cssName: 'sprite-2.css',
+    padding: 10
+  }));
+  return spriteData.pipe(gulp.dest('./dest'));
+});
+gulp.task('sprite-3', function () {
+  let spriteData = gulp.src('src/asset/*-3x.png').pipe(spritesmith({
+    imgName: 'sprite-3.png',
+    cssName: 'sprite-3.css',
+    padding: 10
+  }));
+  return spriteData.pipe(gulp.dest('./dest'));
+});
+
+gulp.task('default', gulp.series('sprite-2', 'sprite-3'));
